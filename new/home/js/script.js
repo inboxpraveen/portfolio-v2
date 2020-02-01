65d12f607f289b5d91fbca4c3438753d935dee32
inboxpraveen <inbox.praveen.kumar17@gmail.com>
2020-02-01 22:21:07 +0530

    update 13 home

--- new/home/js/script.js
+++ new/home/js/script.js
@@ -44,7 +44,7 @@ function reset() {
 }
 function setupHexagons() {
   hexagons = [];
-  let r = 50;
+  let r = 200;
   let R = r / Math.cos(Math.PI / 6);
   let t = r * 2 / Math.sqrt(3);
   let rows = w / (r * 2) + 1;
